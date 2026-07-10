6ce47ed84bc7d45aa0d3499889cfd1cace190898
diabyh599-gif <diabyh599@gmail.com>
2026-07-10 05:30:42 +0000

    Update products.js

--- products.js
+++ products.js
@@ -1,32 +1,81 @@
 const products = [
 {
 id: 1,
-name: "Habit Élégant",
+name: "Ensemble Élégant Premium",
 price: 7500,
-category: "habit",
-event: "fête",
+oldPrice: 9500,
 stock: 5,
 sales: 0,
-image: "https://picsum.photos/300/300?1"
+category: "Vêtements",
+event: "fête",
+rating: 4.8,
+reviews: 124,
+isNew: true,
+isBestSeller: true,
+image: "https://i.ibb.co/0ykTzmM2/IMG-4374.jpg"
 },
+
 {
 id: 2,
-name: "Sneakers Premium",
+name: "Nike Air Force One Blanche",
 price: 15000,
-category: "chaussure",
-event: "sortie",
+oldPrice: 18000,
 stock: 3,
 sales: 0,
-image: "https://picsum.photos/300/300?2"
+category: "Chaussures",
+event: "sortie",
+rating: 4.9,
+reviews: 89,
+isNew: false,
+isBestSeller: true,
+image: "https://i.ibb.co/5WVcQyYX/IMG-4378.jpg"
 },
+
 {
 id: 3,
-name: "Montre Premium",
-price: 6500,
-category: "accessoire",
-event: "fête",
+name: "Samsung Galaxy Buds 3 Pro",
+price: 25000,
+oldPrice: 30000,
 stock: 4,
 sales: 0,
-image: "https://picsum.photos/300/300?3"
+category: "Électronique",
+event: "quotidien",
+rating: 4.7,
+reviews: 256,
+isNew: true,
+isBestSeller: false,
+image: "https://i.ibb.co/M4gzDNK/IMG-4379.jpg"
+},
+
+{
+id: 4,
+name: "Montre POEDAGAR 885",
+price: 6500,
+oldPrice: 8500,
+stock: 6,
+sales: 0,
+category: "Accessoires",
+event: "fête",
+rating: 4.6,
+reviews: 178,
+isNew: false,
+isBestSeller: true,
+image: "https://i.ibb.co/8n9Bxb4C/IMG-4376.jpg"
+},
+
+{
+id: 5,
+name: "Sac pour Femme",
+price: 12000,
+oldPrice: 15000,
+stock: 4,
+sales: 0,
+category: "Accessoires",
+event: "sortie",
+rating: 4.8,
+reviews: 92,
+isNew: true,
+isBestSeller: false,
+image: "https://i.ibb.co/MydF07fD/IMG-4377.jpg"
 }
 ];
