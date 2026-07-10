3f6d347c407fbd79641657c4fbc8c542b382cf53
diabyh599-gif <diabyh599@gmail.com>
2026-07-10 05:00:25 +0000

    Update products.js

--- products.js
+++ products.js
@@ -3,7 +3,8 @@ const products = [
 id: 1,
 name: "Habit Élégant",
 price: 7500,
-category: "fête",
+category: "habit",
+event: "fête",
 image: "https://picsum.photos/300/300?1"
 },
 {
@@ -11,6 +12,7 @@ id: 2,
 name: "Sneakers Premium",
 price: 15000,
 category: "chaussure",
+event: "sortie",
 image: "https://picsum.photos/300/300?2"
 },
 {
@@ -18,6 +20,7 @@ id: 3,
 name: "Montre Premium",
 price: 6500,
 category: "accessoire",
+event: "fête",
 image: "https://picsum.photos/300/300?3"
 }
 ];
