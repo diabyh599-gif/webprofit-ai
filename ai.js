ba9063ecce06a7b871f7f4ad73c94c268e9681d1
diabyh599-gif <diabyh599@gmail.com>
2026-07-10 04:38:24 +0000

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,1 +1,31 @@
-r
+function askAI(){
+
+const question =
+document.getElementById("ai-input")
+.value
+.toLowerCase();
+
+const result =
+document.getElementById("ai-result");
+
+if(question.includes("fête")){
+result.innerHTML =
+"🎉 Je recommande l'Habit Élégant.";
+return;
+}
+
+if(question.includes("chaussure")){
+result.innerHTML =
+"👟 Je recommande les Sneakers Premium.";
+return;
+}
+
+if(question.includes("montre")){
+result.innerHTML =
+"⌚ Je recommande la Montre Premium.";
+return;
+}
+
+result.innerHTML =
+"🤖 Je cherche le meilleur produit pour toi.";
+}
