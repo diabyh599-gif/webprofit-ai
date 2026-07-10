7c76fa0cff78fd655380341f2be45f8dac5101e3
diabyh599-gif <diabyh599@gmail.com>
2026-07-10 04:38:49 +0000

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,1 +1,13 @@
-r
+let cart = [];
+
+function addToCart(productId){
+
+const product =
+products.find(
+p => p.id === productId
+);
+
+cart.push(product);
+
+alert(product.name + " ajouté au panier");
+}
