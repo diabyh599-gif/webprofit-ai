fa22cc0db0d964c3423fb91c4d29c13d0f96d832
diabyh599-gif <diabyh599@gmail.com>
2026-07-30 20:55:06 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,23 @@
-// ==========================================
-// FIREBASE.JS - Version CDN (pour GitHub Pages)
-// ==========================================
-
 // Configuration Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyARFxPYVnUpFvUdmCMMgOspOMGSX_Y26Pg",
-    authDomain: "webprofitai.firebaseapp.com",
-    projectId: "webprofitai",
-    storageBucket: "webprofitai.firebasestorage.app",
-    messagingSenderId: "29810089088",
-    appId: "1:29810089088:web:ca3923e23a62779d1d1a65",
-    measurementId: "G-E6SHYTKE8F"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
-// Initialiser Firebase
+// Initialisation
 firebase.initializeApp(firebaseConfig);
 
-// Services disponibles
+// Services
 const auth = firebase.auth();
 const db = firebase.firestore();
-const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
 
-console.log('🔥 Firebase connecté avec succès !');
+// Provider Google
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
